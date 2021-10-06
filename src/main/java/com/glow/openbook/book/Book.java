841edf3f82a59afee2aede8c5f2e3f64b9235726
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-06 22:53:38 +0900

    feature: implement Book entity class and BookRepository interface

--- src/main/java/com/glow/openbook/book/Book.java
+++ src/main/java/com/glow/openbook/book/Book.java
@@ -1,2 +1,23 @@
-package com.glow.openbook.book;public class Book {
+package com.glow.openbook.book;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Book {
+    @Id
+    private String isbn;
+
+    private String title;
+    private String author;
+    private String publisher;
+    private String description;
+    private String smallCoverUrl;
+    private String largeCoverUrl;
 }
--- src/main/java/com/glow/openbook/book/BookRepository.java
+++ src/main/java/com/glow/openbook/book/BookRepository.java
@@ -1,2 +1,6 @@
-package com.glow.openbook.book;public class BookRepository {
+package com.glow.openbook.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, String> {
 }
