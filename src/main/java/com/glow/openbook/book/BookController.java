014624de31c221bef4d11bb82a5ed8a560f32313
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-06 23:17:45 +0900

    feature: implement BookController and its test
    BookController handles all requests below `/book`. Currently, it handles only get requests that looks up a book by its ISBN.

--- src/main/java/com/glow/openbook/book/BookController.java
+++ src/main/java/com/glow/openbook/book/BookController.java
@@ -1,2 +1,29 @@
-package com.glow.openbook.book;public class BookController {
+package com.glow.openbook.book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("book")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/{isbn}")
+    public Book getBookByIsbn(@PathVariable("isbn") String isbn) throws Exception {
+        Optional<Book> book = bookRepository.findById(isbn);
+        if (book.isPresent()) {
+            return book.get();
+        } else {
+            throw new Exception("Not found");
+        }
+    }
 }
