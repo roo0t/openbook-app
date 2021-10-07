e3242662fa476555773d7f0724aefe67d5139fef
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-07 20:18:26 +0900

    bugfix: add NoArgsConstructor annotation to ReadingGroup entity

--- src/main/java/com/glow/openbook/readinggroup/ReadingGroup.java
+++ src/main/java/com/glow/openbook/readinggroup/ReadingGroup.java
@@ -3,6 +3,7 @@ package com.glow.openbook.readinggroup;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReadingGroup {
 
