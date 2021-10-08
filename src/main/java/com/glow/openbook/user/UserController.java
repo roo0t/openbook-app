30cea988fb9b9008c74367aa18d99f6300399a3a
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-08 14:12:08 +0900

    feature: add UserController and UserRepository

---
+++ src/main/java/com/glow/openbook/user/UserController.java
@@ -0,0 +1,13 @@
+package com.glow.openbook.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class UserController {
+
+}
---
+++ src/main/java/com/glow/openbook/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.glow.openbook.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
