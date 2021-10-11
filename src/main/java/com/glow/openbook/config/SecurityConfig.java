17f6cc7472cafac21284cce1d4e5095aea5d5c1d
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-11 17:07:31 +0900

    feature: add security configuration to require any requests to be authenticated and to enable form login

--- src/main/java/com/glow/openbook/config/SecurityConfig.java
+++ src/main/java/com/glow/openbook/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.glow.openbook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,5 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin();
     }
 }
