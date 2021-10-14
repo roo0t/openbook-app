8c7bfeabcdac89c743563d37892652ffcee3f741
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-15 00:37:50 +0900

    feature: explicitly permit all access to login form

--- src/main/java/com/glow/openbook/config/SecurityConfig.java
+++ src/main/java/com/glow/openbook/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                    .permitAll();
     }
 }
