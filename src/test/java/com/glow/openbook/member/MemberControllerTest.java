3f1c5cb1b301b6575342594f47a3eeddf4dc88ee
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2021-10-15 00:37:27 +0900

    test: set up stand-alone mock MVC for MemberControllerTest and write testMyInfo()

--- src/test/java/com/glow/openbook/member/MemberControllerTest.java
+++ src/test/java/com/glow/openbook/member/MemberControllerTest.java
@@ -1,23 +1,62 @@
 package com.glow.openbook.member;
 
-import com.glow.openbook.user.MemberRepository;
+import com.glow.openbook.user.MemberController;
 import com.glow.openbook.user.MemberService;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest
 public class MemberControllerTest {
-
-    @Autowired
-    private MemberRepository memberRepository;
+    private MockMvc mockMvc;
 
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private MemberController memberController;
+
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = standaloneSetup(memberController)
+                .apply(springSecurity(springSecurityFilterChain))
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .defaultRequest(get("/member").accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+                .build();
+    }
+
     @Test
-    public void testMyinfo() {
-        memberService.register("test@glowingreaders.club", "abcd1234");
+    public void testMyinfo() throws Exception {
+        final String emailAddress = "test@glowingreaders.club";
+        final String plainPassword = "abcd1234";
+        memberService.register(emailAddress, plainPassword);
+
+        final MockHttpSession session = (MockHttpSession)mockMvc
+                .perform(formLogin().user(emailAddress).password(plainPassword))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(authenticated())
+                .andReturn().getRequest().getSession();
+        mockMvc.perform(get("/member").session(session).with(csrf().asHeader()))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.statusMessage", is("SUCCESS")))
+                .andExpect(jsonPath("$.data.emailAddress", is(emailAddress)));
     }
 }
