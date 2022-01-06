93a33f1ed511424e99d6b051dcbdd124fb2a9e30
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2022-01-06 20:31:50 +0900

    feature: add getByIsbn method to BookRepository

--- backend/src/main/java/com/glow/openbook/book/BookRepository.java
+++ backend/src/main/java/com/glow/openbook/book/BookRepository.java
@@ -3,4 +3,5 @@ package com.glow.openbook.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, String> {
+    Book getByIsbn(String isbn);
 }
