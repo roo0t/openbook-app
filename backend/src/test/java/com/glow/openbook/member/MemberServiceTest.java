05d843d318afac6f4aeba800f679a0b7b38357a6
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2022-01-08 23:47:50 +0900

    test: write a test for sign-up functionality in MemberService

---
+++ backend/src/test/java/com/glow/openbook/member/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package com.glow.openbook.member;
+
+import com.glow.openbook.user.Member;
+import com.glow.openbook.user.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class MemberServiceTest {
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void signUpTest() {
+        String emailAddress = "sign-up-test@glowingreaders.club";
+        String password = "password";
+
+        assertThat(memberService.getMember(emailAddress)).isEmpty();
+        Member member = memberService.register(emailAddress, password);
+        assertThat(member.getEmailAddress()).isEqualTo(emailAddress);
+        assertThat(member.getPassword()).isNotEqualTo(password);
+        assertThat(member.getNickname()).isNotBlank();
+
+        Optional<Member> loadedMember = memberService.getMember(emailAddress);
+        assertThat(loadedMember).isNotEmpty();
+        assertThat(loadedMember.get()).usingRecursiveComparison().isEqualTo(member);
+    }
+}
