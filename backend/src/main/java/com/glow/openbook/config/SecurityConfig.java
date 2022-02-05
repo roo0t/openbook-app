2042bf7e4c62903f1090d1deaabe70df3767d2c2
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2022-02-05 22:35:11 +0900

    feature: set SpringSecurity session management policy to STATELESS

--- backend/src/main/java/com/glow/openbook/config/SecurityConfig.java
+++ backend/src/main/java/com/glow/openbook/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -38,6 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .antMatchers("/member/signin").permitAll()
                     .anyRequest().authenticated()
+                    .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and()
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtProvider),
