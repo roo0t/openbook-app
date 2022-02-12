e6b7d48ce6aa948e06b64433274d8e99ad7460f8
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2022-02-12 23:32:11 +0900

    refactor: rename signUpTest to represent its purpose better and change it to use mock repository

--- backend/src/test/java/com/glow/openbook/member/MemberServiceTest.java
+++ backend/src/test/java/com/glow/openbook/member/MemberServiceTest.java
@@ -1,35 +1,52 @@
 package com.glow.openbook.member;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class MemberServiceTest {
-    @Autowired
-    private MemberService memberService;
+    @Mock
+    private MemberRepository memberRepository;
 
     @Autowired
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
     private PasswordEncoder passwordEncoder;
 
     @Test
-    public void signUpTest() {
-        String emailAddress = "sign-up-test@glowingreaders.club";
-        String password = "password";
+    public void register_adds_member_to_repository_with_password_encoded() {
+        // Arrange
+        final String emailAddress = "sign-up-test@glowingreaders.club";
+        final String password = "password";
+        final String encodedPassword = "encodedPassword";
+        when(memberRepository.save(any()))
+                .thenAnswer(invocation -> invocation.getArguments()[0]);
+        when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
 
-        assertThat(memberService.getMember(emailAddress)).isEmpty();
+        // Action
         Member member = memberService.register(emailAddress, password);
-        assertThat(member.getEmailAddress()).isEqualTo(emailAddress);
-        assertThat(member.getPassword()).isNotEqualTo(password);
-        assertThat(member.getNickname()).isNotBlank();
 
-        Optional<Member> loadedMember = memberService.getMember(emailAddress);
-        assertThat(loadedMember).isNotEmpty();
-        assertThat(loadedMember.get()).usingRecursiveComparison().isEqualTo(member);
+        // Assert
+        verify(memberRepository, times(1))
+                .save(argThat(m -> m.getEmailAddress().equals(emailAddress)));
+        verifyNoMoreInteractions(memberRepository);
+
+        assertThat(member.getEmailAddress()).isEqualTo(emailAddress);
+        assertThat(member.getPassword()).isEqualTo(encodedPassword);
+        assertThat(member.getNickname()).isNotBlank();
     }
 }
