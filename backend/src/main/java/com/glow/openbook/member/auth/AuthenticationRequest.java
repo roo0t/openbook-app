64b7d3f9625c282d3270ad7ceab769f2d85796ea
Daekeun Lee <daekeun.lee@kaist.ac.kr>
2022-02-18 20:19:14 +0900

    refactor: annotate AuthenticationRequest with lombok annotations to remove verbose getter and setter methods

--- backend/src/main/java/com/glow/openbook/member/auth/AuthenticationRequest.java
+++ backend/src/main/java/com/glow/openbook/member/auth/AuthenticationRequest.java
@@ -1,20 +1,15 @@
 package com.glow.openbook.member.auth;
 
-public class AuthenticationRequest {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
     private String emailAddress;
     private String password;
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
